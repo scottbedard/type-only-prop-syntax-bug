660282c07f56d69349ac28aac135ca7f8e8e2124
Scott Bedard <scottbedard90@gmail.com>
2021-11-14 11:02:43 -0600

    bug demonstration

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Child />
+  <Child checked />
+</template>
+
+<script lang="ts" setup>
+import Child from './Child.vue'
+</script>
---
+++ src/Child.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <pre :style="{ color: checked ? 'green' : 'red' }">{{ { checked } }}</pre>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  checked?: boolean
+}>()
+</script>
